a8107d02bc27a409dafe68925058ace976907a1a
karlygash7 <karlygash.dev@gmail.com>
2023-01-17 10:17:30 +0600

    add homework-03 files

---
+++ index.js
@@ -0,0 +1,11 @@
+// Тренируемся общаться с пользователем посредством alert / confirm / prompt.
+
+let user = prompt('What is your name?');
+let age = prompt('How old are you?');
+let gender = prompt('Female/Male');
+console.log(user);
+console.log(age);
+console.log(gender);
+
+
+
---
+++ operator.js
@@ -0,0 +1,10 @@
+// Работа с операторами
+
+let favoriteNumber = prompt('What is your favorite number?');
+
+if (favoriteNumber === 7){
+    alert('7');
+} else {
+    alert('Sorry');
+};
+
